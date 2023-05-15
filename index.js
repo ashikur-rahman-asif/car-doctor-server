392e49122838d81b926482a450bc403afc75d664
asif247 <86229363+asif247@users.noreply.github.com>
2023-05-16 00:48:22 +0600

    patch

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ async function run() {
             // sort returned documents in ascending order by title (A->Z)
             // sort: { title: 1 },
             // Include only the `title` and `imdb` fields in each returned document
-            projection: { title: 1, price: 1,service_id:1 },
+            projection: { title: 1, price: 1,service_id:1, img:1 },
           };
           const result = await serviceCollection.findOne(query,options)
           res.send(result)
@@ -55,6 +55,35 @@ async function run() {
         const result = await bookingCollection.insertOne(booking)
         res.send(result)
     })
+    app.get('/bookings', async (req, res) => {
+      console.log(req.query.email);
+      let query = {};
+      if (req.query?.email) {
+          query = { email: req.query.email }
+      }
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+    })
+    app.patch('/bookings/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedBooking = req.body;
+      console.log(updatedBooking);
+      const updateDoc = {
+          $set: {
+              status: updatedBooking.status
+          },
+      };
+      const result = await bookingCollection.updateOne(filter, updateDoc);
+      res.send(result);
+  })
+
+  app.delete('/bookings/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) }
+    const result = await bookingCollection.deleteOne(query);
+    res.send(result);
+})
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
